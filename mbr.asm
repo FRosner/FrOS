bc5ffce52085ebfb1efb47dd9feb28ff10564953
Frank Rosner <frank@fam-rosner.de>
2020-10-28 19:04:57 +0100

    print X through derefencing memory

--- mbr.asm
+++ mbr.asm
@@ -1,16 +1,19 @@
-mov ah, 0x0e ; tty mode http://vitaly_filatov.tripod.com/ng/asm/asm_023.15.html
-mov al, 'H'
-int 0x10 ; https://en.wikipedia.org/wiki/INT_10H
-mov al, 'e'
+[org 0x7c00]
+mov ah, 0x0e
+
+; Having solved the memory offset problem with 'org', this is now the correct answer
+mov al, "2"
 int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al
-mov al, 'o'
+mov al, [the_secret]
 int 0x10
 
-jmp $ ; jump to current address = infinite loop
+jmp $ ; infinite loop
 
-; padding and magic number
-times 510 - ($-$$) db 0
-dw 0xaa55 
+the_secret:
+    ; ASCII code 0x58 ('X') is stored just before the zero-padding.
+    ; On this code that is at byte 0x2d (check it out using 'xxd file.bin')
+    db "X"
+
+; zero padding and magic bios number
+times 510-($-$$) db 0
+dw 0xaa55
