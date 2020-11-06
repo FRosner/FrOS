0338051c2c5e8dab5b1dc0f400b456e68143fc95
Frank Rosner <frank@fam-rosner.de>
2020-11-06 15:07:07 +0100

    remove print stuff

--- disk.asm
+++ disk.asm
@@ -28,19 +28,10 @@ disk_load:
 
 
 disk_error:
-    mov bx, DISK_ERROR
-    call print16
-    call print16_nl
-    mov dh, ah ; ah = error code, dl = disk drive that dropped the error
-    call print16_hex ; check out the code at http://stanislavs.org/helppc/int_13-1.html
     jmp disk_loop
 
 sectors_error:
-    mov bx, SECTORS_ERROR
-    call print16
+    jmp disk_loop
 
 disk_loop:
     jmp $
-
-DISK_ERROR: db "Disk read error", 0
-SECTORS_ERROR: db "Incorrect number of sectors read", 0
--- mbr.asm
+++ mbr.asm
@@ -1,30 +1,22 @@
+[bits 16]
 [org 0x7c00]
+
 KERNEL_OFFSET equ 0x1000 ; The same one we used when linking the kernel
 
 mov [BOOT_DRIVE], dl ; Remember that the BIOS sets us the boot drive in 'dl' on boot
 mov bp, 0x9000
 mov sp, bp
 
-mov bx, MSG_16BIT_MODE
-call print16
-call print16_nl
-
 call load_kernel ; read the kernel from disk
 call switch_to_32bit ; disable interrupts, load GDT,  etc. Finally jumps to 'BEGIN_PM'
 jmp $ ; Never executed
 
-%include "print-16bit.asm"
-%include "print-32bit.asm"
 %include "disk.asm"
 %include "gdt.asm"
 %include "switch-to-32bit.asm"
 
 [bits 16]
 load_kernel:
-    mov bx, MSG_LOAD_KERNEL
-    call print16
-    call print16_nl
-
     mov bx, KERNEL_OFFSET ; Read from disk and store in 0x1000
     mov dh, 2
     mov dl, [BOOT_DRIVE]
@@ -33,16 +25,11 @@ load_kernel:
 
 [bits 32]
 BEGIN_32BIT:
-    mov ebx, MSG_32BIT_MODE
-    call print32
     call KERNEL_OFFSET ; Give control to the kernel
     jmp $ ; Stay here when the kernel returns control to us (if ever)
 
 
 BOOT_DRIVE db 0 ; It is a good idea to store it in memory because 'dl' may get overwritten
-MSG_16BIT_MODE db "Started in 16-bit Real Mode", 0
-MSG_32BIT_MODE db "Landed in 32-bit Protected Mode", 0
-MSG_LOAD_KERNEL db "Loading kernel into memory", 0
 
 ; padding
 times 510 - ($-$$) db 0
