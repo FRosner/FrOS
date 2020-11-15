411510e2f206a6b1ffb4e9ae1f706ac9c6cfa2dc
Frank Rosner <frank@fam-rosner.de>
2020-11-15 17:10:40 +0100

    send EOI at the end

--- cpu/isr.c
+++ cpu/isr.c
@@ -129,16 +129,15 @@ void register_interrupt_handler(uint8_t n, isr_t handler) {
 }
 
 void irq_handler(registers_t *r) {
-    /* After every interrupt we need to send an EOI to the PICs
-     * or they will not send another interrupt again */
-    if (r->int_no >= 40) {
-        port_byte_out(0xA0, 0x20); /* follower */
-    }
-    port_byte_out(0x20, 0x20); /* leader */
-
     /* Handle the interrupt in a more modular way */
     if (interrupt_handlers[r->int_no] != 0) {
         isr_t handler = interrupt_handlers[r->int_no];
         handler(r);
     }
+
+    // EOI
+    if (r->int_no >= 40) {
+        port_byte_out(0xA0, 0x20); /* follower */
+    }
+    port_byte_out(0x20, 0x20); /* leader */
 }
