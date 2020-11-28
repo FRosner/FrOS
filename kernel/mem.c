4b076cd89987297f5efd488e623ccd035b0f5585
Frank Rosner <frank@fam-rosner.de>
2020-11-28 08:14:39 +0100

    refactor

--- kernel/mem.c
+++ kernel/mem.c
@@ -17,7 +17,7 @@ void memory_copy(uint8_t *source, uint8_t *dest, uint32_t nbytes) {
  * For further details, see http://www.sunshine2k.de/license.html.
  */
 #define DYNAMIC_MEM_TOTAL_SIZE 4*1024
-#define dynamic_mem_node_t_SIZE sizeof(dynamic_mem_node_t)
+#define DYNAMIC_MEM_NODE_SIZE sizeof(dynamic_mem_node_t)
 
 typedef struct dynamic_mem_node {
     uint32_t size;
@@ -31,7 +31,7 @@ static dynamic_mem_node_t *dynamic_mem_start;
 
 void init_dynamic_mem() {
     dynamic_mem_start = (dynamic_mem_node_t *) dynamic_mem_area;
-    dynamic_mem_start->size = DYNAMIC_MEM_TOTAL_SIZE - dynamic_mem_node_t_SIZE;
+    dynamic_mem_start->size = DYNAMIC_MEM_TOTAL_SIZE - DYNAMIC_MEM_NODE_SIZE;
     dynamic_mem_start->next = NULL_POINTER;
     dynamic_mem_start->prev = NULL_POINTER;
 }
@@ -46,7 +46,7 @@ void *mem_alloc(size_t size) {
     while (current_mem_block) {
         // check if block can be used and is smaller than current best
         if ((!current_mem_block->used) &&
-            (current_mem_block->size >= (size + dynamic_mem_node_t_SIZE)) &&
+            (current_mem_block->size >= (size + DYNAMIC_MEM_NODE_SIZE)) &&
             (current_mem_block->size <= best_mem_block_size)) {
             // update best block
             best_mem_block = current_mem_block;
@@ -60,11 +60,11 @@ void *mem_alloc(size_t size) {
     // check if we actually found a matching (free, large enough) block
     if (best_mem_block != NULL_POINTER) {
         // subtract newly allocated memory (incl. size of the mem node) from selected block
-        best_mem_block->size = best_mem_block->size - size - dynamic_mem_node_t_SIZE;
+        best_mem_block->size = best_mem_block->size - size - DYNAMIC_MEM_NODE_SIZE;
 
         // create new mem node after selected node, effectively splitting the memory region
         dynamic_mem_node_t *mem_node_allocate = (dynamic_mem_node_t *) (((uint8_t *) best_mem_block) +
-                                                                        dynamic_mem_node_t_SIZE +
+                                                                        DYNAMIC_MEM_NODE_SIZE +
                                                                         best_mem_block->size);
         mem_node_allocate->size = size;
         mem_node_allocate->used = true;
@@ -78,7 +78,7 @@ void *mem_alloc(size_t size) {
         best_mem_block->next = mem_node_allocate;
 
         // return pointer to newly allocated memory (right after the new list node)
-        return (void *) ((uint8_t *) mem_node_allocate + dynamic_mem_node_t_SIZE);
+        return (void *) ((uint8_t *) mem_node_allocate + DYNAMIC_MEM_NODE_SIZE);
     }
 
     return NULL_POINTER;
@@ -91,7 +91,7 @@ void mem_free(void *p) {
     }
 
     // get mem node associated with pointer
-    dynamic_mem_node_t *current_mem_node = (dynamic_mem_node_t *) ((uint8_t *) p - dynamic_mem_node_t_SIZE);
+    dynamic_mem_node_t *current_mem_node = (dynamic_mem_node_t *) ((uint8_t *) p - DYNAMIC_MEM_NODE_SIZE);
 
     // pointer we're trying to free was not dynamically allocated it seems
     if (current_mem_node == NULL_POINTER) {
@@ -106,7 +106,7 @@ void mem_free(void *p) {
     if (next_mem_node != NULL_POINTER && !next_mem_node->used) {
         // add size of next block to current block
         current_mem_node->size += current_mem_node->next->size;
-        current_mem_node->size += dynamic_mem_node_t_SIZE;
+        current_mem_node->size += DYNAMIC_MEM_NODE_SIZE;
 
         // remove next block from list
         current_mem_node->next = current_mem_node->next->next;
@@ -120,7 +120,7 @@ void mem_free(void *p) {
     if (prev_mem_node != NULL_POINTER && !prev_mem_node->used) {
         // add size of previous block to current block
         prev_mem_node->size += current_mem_node->size;
-        prev_mem_node->size += dynamic_mem_node_t_SIZE;
+        prev_mem_node->size += DYNAMIC_MEM_NODE_SIZE;
 
         // remove current node from list
         prev_mem_node->next = current_mem_node->next;
